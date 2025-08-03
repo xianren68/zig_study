865f894f0f57b426a890542b068f4189aaa0c526
xianren <xianren711@qq.com>
2025-08-03 15:31:56 +0800

    update

--- parseIni/src/lib.zig
+++ parseIni/src/lib.zig
@@ -1,8 +1,72 @@
 const std = @import("std");
+pub const IniError = error{
+    InvalidKey,
+    InvalidValue,
+    SectionNotFound,
+    InvalidType,
+    NotFoundKey,
+    NotFoundSection,
+    MissingField,
+};
+const Section = struct {
+    name: []const u8,
+    keys: std.StringHashMap([]const u8),
+    pub fn key(self: *Section, k: []const u8) ?[]const u8 {
+        return self.keys.get(k);
+    }
+    pub fn bind_model(self: *Section, comptime T: type) !T {
+        const type_info = @typeInfo(T);
+        if (type_info != .Struct) {
+            return IniError.InvalidType;
+        }
+        const fields = type_info.Struct.fields;
+        var config: T = undefined;
+        inline for (fields) |field| {
+            const field_name = field.name;
+            const field_type = field.type;
+            const field_value = self.key(field_name);
+            if (field_value == null) {
+                if (field.default_value) |default_ptr| {
+                    // 将anyopaque指针转换为具体类型的指针
+                    const default_value_ptr: *const field_type = @ptrCast(@alignCast(default_ptr));
+                    // 然后赋值：将默认值赋给字段
+                    @field(config, field.name) = default_value_ptr.*;
+                } else {
+                    return IniError.MissingField;
+                }
+            } else {
+                try assign_field(&@field(config, field_name), field_type, field_value.?);
+            }
+        }
+        return config;
+    }
+    fn assign_field(field_ptr: anytype, comptime FieldType: type, value: []const u8) !void {
+        const file_info = @typeInfo(FieldType);
+        switch (file_info) {
+            .Int => {
+                field_ptr.* = try std.fmt.parseInt(FieldType, value, 10);
+            },
+            .Float => {
+                field_ptr.* = try std.fmt.parseFloat(FieldType, value);
+            },
+            .Bool => {
+                field_ptr.* = std.mem.eql(u8, value, "true") or
+                    std.mem.eql(u8, value, "1") or
+                    std.mem.eql(u8, value, "yes");
+            },
+            .Pointer => |ptr_info| {
+                if (ptr_info.size != .Slice or ptr_info.child != u8 or !ptr_info.is_const) {
+                    return IniError.InvalidType;
+                }
+                field_ptr.* = value;
+            },
+            else => return IniError.InvalidType,
+        }
+    }
+};
 
 pub const IniConfig = struct {
     data: std.StringHashMap(std.StringHashMap([]const u8)),
-    current_section: []const u8,
     pub fn Parse(path: []const u8) !IniConfig {
         const file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
         defer file.close();
@@ -10,33 +74,32 @@ pub const IniConfig = struct {
         var reader = buf_reader.reader();
         var ini_config = IniConfig{
             .data = std.StringHashMap(std.StringHashMap([]const u8)).init(std.heap.page_allocator),
-            .current_section = "",
         };
-        try ini_config.data.put(ini_config.current_section, std.StringHashMap([]const u8).init(std.heap.page_allocator));
+        var current_section: []const u8 = "";
+        try ini_config.data.put("", std.StringHashMap([]const u8).init(std.heap.page_allocator));
         // 按行读取
         while (try reader.readUntilDelimiterOrEofAlloc(std.heap.page_allocator, '\n', 1024)) |line| {
             // 处理每一行
             var ln = line[0 .. line.len - 1];
             if (ln.len == 0) continue;
             if (ln[0] == '[') {
-                std.heap.page_allocator.free(ini_config.current_section);
-                ini_config.current_section = ln[1 .. ln.len - 1];
-                try ini_config.data.put(ini_config.current_section, std.StringHashMap([]const u8).init(std.heap.page_allocator));
+                std.heap.page_allocator.free(current_section);
+                current_section = ln[1 .. ln.len - 1];
+                try ini_config.data.put(current_section, std.StringHashMap([]const u8).init(std.heap.page_allocator));
             } else {
                 const index = std.mem.indexOfScalar(u8, ln, '=');
                 if (index == null) continue;
                 const key = ln[0..index.?];
                 const value = ln[index.? + 1 .. ln.len];
-                try ini_config.data.getPtr(ini_config.current_section).?.put(key, value);
+                try ini_config.data.getPtr(current_section).?.put(key, value);
             }
         }
         return ini_config;
     }
-    pub fn Section(self: *IniConfig, name: []const u8) *IniConfig {
-        self.current_section = name;
-        return self;
-    }
-    pub fn Key(self: *IniConfig, key: []const u8) []const u8 {
-        return self.data.get(self.current_section).?.get(key).?;
+    pub fn section(self: *IniConfig, name: []const u8) ?Section {
+        if (self.data.get(name)) |sec| {
+            return Section{ .keys = sec, .name = name };
+        }
+        return null;
     }
 };
--- parseIni/src/test.zig
+++ parseIni/src/test.zig
@@ -1,10 +1,46 @@
 const std = @import("std");
 const testing = std.testing;
-const IniConfig = @import("lib.zig").IniConfig;
+const Ini = @import("lib.zig");
+const IniConfig = Ini.IniConfig;
+const IniError = Ini.IniError;
+const globalConfig = struct {
+    name: []const u8,
+    age: u32,
+    sex: []const u8 = "male",
+    isMarried: bool,
+};
+const baseConfig = struct {
+    name: []const u8,
+    age: u32,
+    sex: []const u8 = "male",
+};
+
+const address = struct {
+    street: []const u8,
+    city: []const u8,
+    state: []const u8,
+    zip: []const u8,
+    is_active: bool,
+};
 
 test "parse ini file" {
     var cfg = try IniConfig.Parse("test.ini");
-    try testing.expectEqualStrings("xianren", cfg.Section("").Key("name"));
-    try testing.expectEqualStrings("25", cfg.Section("").Key("age"));
-    try testing.expectEqualStrings("Anytown", cfg.Section("address").Key("city"));
+    try testing.expect(cfg.section("hello") == null);
+    var base_config = cfg.section("").?;
+    try testing.expectEqualStrings("xianren", base_config.key("name").?);
+    try testing.expectEqualStrings("25", base_config.key("age").?);
+    try testing.expect(base_config.key("hello") == null);
+    var address_config = cfg.section("address").?;
+    try testing.expectEqualStrings("Anytown", address_config.key("city").?);
+    const base_model = try base_config.bind_model(baseConfig);
+    try testing.expectEqualStrings("xianren", base_model.name);
+    try testing.expectEqual(25, base_model.age);
+    try testing.expectEqualStrings("male", base_model.sex);
+    try testing.expectError(IniError.MissingField, base_config.bind_model(globalConfig));
+    const address_model = try address_config.bind_model(address);
+    try testing.expectEqualStrings("123 Main St", address_model.street);
+    try testing.expectEqualStrings("Anytown", address_model.city);
+    try testing.expectEqualStrings("CA", address_model.state);
+    try testing.expectEqualStrings("12345", address_model.zip);
+    try testing.expectEqual(true, address_model.is_active);
 }
